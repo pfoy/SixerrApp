3a8a54cfd7d0c42c89b708e50ddfb6df14f14665
Peter Foy <peter.foy5@gmail.com>
2018-09-04 21:07:57 -0400

    fetch db env directly

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,12 +81,10 @@ WSGI_APPLICATION = 'sixerr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': dj_database_url.config()
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 
 
 # Password validation
